5341c0130208d53460c55527ac79242fb983cca9
VAISHNAVIBANSAL <83929229+VAISHNAVIBANSAL@users.noreply.github.com>
2024-04-16 11:10:59 -0700

    App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
